f2974d8e2f114e5b644cd91222f42cc596600a90
Grace kungu <grayckungu2@gmail.com>
2023-06-15 17:38:22 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (Blocks){
+    if (Blocks===43) {
+    return(43-42);
+    }
+    else {
+    return(50-42);
+    } 
+    } 
+    function distanceFromHqInFeet (Feet) {
+    if (Feet===43) {
+    return 264;
+    }
+    else {
+    return distanceFromHqInBlocks(-2112+2376)*264;
+    }
+    } 
+    function distanceTravelledInFeet(x,y){
+    if (y>x){
+    return((y-x)*264);
+    }
+    else
+    return((x-y)*264);
+    }
+    
+    function calculatesFarePrice(start,destination){
+    const distance = distanceTravelledInFeet(start,destination);
+    if (distance<=400 && distance< 2000){
+    return 0;
+
+    }
+    if (distance>=400 && distance<=2000){
+    return (distance-400)* 0.02;
+    }
+    else if(distance>=2000&& distance<=2500){
+    return 25;
+    }
+    if(distance > 2500){
+    return "cannot travel that far";
+    }
+    
+    }
+    
+    
